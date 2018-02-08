a52fcea9baf2aa926be9cd74b9d0a32147363ad2
Eumin Lee <lee.eumin@gmail.com>
2018-02-08 09:59:33 -0800

    a
    a

---
+++ OX/OX.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class OX here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class OX
+{
+    private String grid[][] = new String[2][2];
+    public void print(){
+        
+    }
+}
